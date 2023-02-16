72803076f22f94af55097b1dd183ae132bf9904c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-16 14:01:58 +0600

    added work section images

--- src/Pages/Home/Works/Works.js
+++ src/Pages/Home/Works/Works.js
@@ -1,25 +1,74 @@
 import React from 'react';
 import doctor from '../../../asset/doctor.png';
+import './Works.css';
 
 const Works = () => {
     return (
         <div>
             <h2 className='text-4xl font-bold'>Some Works of Mine</h2>
-            <div className='flex flex-row-reverse'>
+            {/* <div className='parent flex-row-reverse'>
             <div className=''>
                 <img src={doctor} alt="" srcset="" />
             </div>
             <div className=''>
                 <h5 className='text-xl font-bold text-primary'>Featured Project</h5>
                 <h4 className='text-2xl font-bold text-secondary'>Doctors Portal</h4>
-                <div data-theme='dark' className=''>
+                <div data-theme='dark' className='child'>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quibusdam et odit, modi harum voluptatem blanditiis necessitatibus consectetur iste nam qui facere neque delectus doloremque eos maxime quos dicta tempora cumque voluptates labore impedit, hic omnis. Voluptatibus quia obcaecati, magni tempora, inventore error adipisci in nesciunt, dolores deleniti maiores facilis ut molestias rem quo architecto iste sint ducimus.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quibusdam et odit, modi harum voluptatem blanditiis necessitatibus consectetur iste nam qui facere neque delectus doloremque eos maxime quos dicta tempora cumque voluptates labore impedit, hic omnis. Voluptatibus quia obcaecati, magni tempora, inventore error adipisci in nesciunt.
                     
                 </p>
                 </div>
             </div>
-            </div>
+            </div> */}
+
+<div className="grid gap-4 grid-cols-2">
+
+<div className="hero lg:h-96 rounded-lg" style={{ backgroundImage: `url(${doctor})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div className="hero lg:h-96 rounded-lg" style={{ backgroundImage: `url(${doctor})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div className="hero lg:h-96 rounded-lg" style={{ backgroundImage: `url(${doctor})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div className="hero lg:h-96 rounded-lg" style={{ backgroundImage: `url(${doctor})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+</div>
         </div>
     );
 };
